e4e1da6d80af8feb94a061325a43b2e3549176ec
Andre Praeiro <andrepraeiro@gmail.com>
2020-04-27 23:36:41 -0400

    creating relationship with models

--- src/database/migrations/1588040712959-CreateUsers.ts
+++ src/database/migrations/1588040712959-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1588040712959 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
