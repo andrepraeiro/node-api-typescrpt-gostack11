a69128671b01a043c9f34f6899625575178b99a0
Andre Praeiro <andrepraeiro@gmail.com>
2020-04-28 15:44:33 -0400

    generate token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 export default class AuthenticateUserService {
   public async execute({ email, password }: Request): Promise<Response> {
@@ -25,6 +28,14 @@ export default class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    return { user };
+    const token = sign({}, '24008ed6c86f7f9d2fc8985872c38a2a', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
