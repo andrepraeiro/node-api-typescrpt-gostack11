e2934e7d4b93323d92323bea3579dc5b3c4916d3
Andre Praeiro <andrepraeiro@gmail.com>
2020-05-03 00:00:34 -0400

    add error handling

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -4,6 +4,8 @@ import { getRepository } from 'typeorm';
 import User from '../models/User';
 import uploadConfig from '../config/upload';
 
+import AppError from '../errors/AppError';
+
 interface Request {
   user_id: string;
   avatarFilename: string;
@@ -14,7 +16,7 @@ export default class UpdateUserAvatarService {
     const user = await userRepository.findOne(user_id);
 
     if (!user) {
-      throw new Error('Only athenticated user can change avatar.');
+      throw new AppError('Only athenticated user can change avatar.', 401);
     }
 
     if (user.avatar) {
